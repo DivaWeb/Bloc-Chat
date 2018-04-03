6cc3b38526471ad45c422216109c336ff0a053ab
Lanitta <lanittad@hotmail.com>
2018-04-03 08:15:52 -0700

    working code

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -13,51 +13,49 @@ class MessageList extends React.Component {
 
    }
 
-   componentDidMount(){
-     this.messagesRef.on('child_added', snapshot => {
+  componentDidMount(){
+     this.messagesRef.on( 'child_added', snapshot => {
        const message = snapshot.val();
        message.key = snapshot.key;
-       this.setState({ messages: this.state.messages.concat( message ) });
-     });
-   }
+       this.setState( { messages: this.state.messages.concat( message ) } ,  () => { this.updateDisplayedMessages( this.props.activeRoom ) } )
+   });
+ }
 
    componentWillReceiveProps(nextProps) {
      this.updateDisplayedMessages( nextProps.activeRoom );
    }
 
-  createMessage(e) {
-  //  if(!this.props.activeRoom || !newMessageText) { return }
+   /*const timeRef = firebase.database().ref("sessions"),
+   timeRef.push({startedAt: firebase.database.ServerValue.TIMESTAMP});*/
+  
+
+
+  createMessage(e) {+
   e.preventDefault();
-  console.log(this.props.activeRoom);
     this.messagesRef.push({
       Content: this.state.newMessageText,
       sendAt: this.props.firebase.database.ServerValue.TIMESTAMP,
       roomId: this.props.activeRoom.key,
       username: this.props.user ? this.props.user.displayName : 'Guest'
-
     });
-
-
     this.setState({ newMessageText: '' });
-    this.updateDisplayedMessages(this.props.activeRoom);
-
   }
 
 
-  handleChange(e) {
-    this.setState({newMessageText: e.target.value});
 
+ handleChange(e) {
+    this.setState( { newMessageText: e.target.value } );
   }
 
-  updateDisplayedMessages(activeRoom) {
-    if(!activeRoom) { return }
-    this.setState({ displayedMessages: this.state.messages.filter( message => {console.log(message.key, activeRoom.key); message.key === activeRoom.key })})
+updateDisplayedMessages( activeRoom ) {
+    if ( !activeRoom ) { return }
+    this.setState( { displayedMessages: this.state.messages.filter( message => message.roomId === activeRoom.key ) })
   }
 
-  checkForNewMessages(){
-    this.messagesRef.on('child_added', snapshot =>{
-      const message = Object.assign(snapshot.val(), {key: snapshot.key})
-      this.setState({messages: this.state.messages.filter( message => message.key !== snapshot.key) }, () =>{
+ checkForNewMessages(){
+    this.messagesRef.on ( 'child_added', snapshot =>{
+      const message = Object.assign( snapshot.val(), { key: snapshot.key } )
+      this.setState( { messages: this.state.messages.filter( message => message.key !== snapshot.key ) }, () => {
         this.updateDisplayedmessages( this.props.activeRoom)
       });
     });
@@ -68,14 +66,16 @@ class MessageList extends React.Component {
    render(){
      return(
      <div>
-       <h2 > { this.props.activeRoom ? this.props.activeRoom.name : ''}</h2>
+       <h2> { this.props.activeRoom ? this.props.activeRoom.name : '' } </h2>
      <ul>
 
-     { this.state.displayedMessages.map((message) =>
-        <li key = {message.key}>
-          <div>{message.Content}</div>
-          <div>{message.username}</div>
-          <div> {message.sendAt}</div>
+     { this.state.displayedMessages.map(  ( message ) =>
+        <li key = { message.key } >
+          <div> { message.Content } </div>
+
+          <div> { message.username } </div>
+
+          <div span = "span" format = "MM/DD/YY hh:mm "> { message.sendAt / 1000 } </div>
 
         </li>
       )}
